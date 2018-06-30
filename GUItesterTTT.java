23adf2e15d5335cd0107d2e10230ace88fe14be9
Timothy Negron <37198273+negrt@users.noreply.github.com>
2018-06-30 15:16:09 -0400

    Add files via upload
    First version of Tic-Tac-Toe game. Intentions to add a leader board and player class that saves Gamertags, wins, and losses.

---
+++ GUItesterTTT.java
@@ -0,0 +1,18 @@
+package guiTTT;
+
+import javax.swing.SwingUtilities;
+
+public class GUItesterTTT {
+
+	public static void main(String[] args) 
+	{
+			SwingUtilities.invokeLater(new Runnable()
+			{
+				@Override
+				public void run()
+				{
+					new GUIframeTTT();
+				}
+			});
+	}
+}
